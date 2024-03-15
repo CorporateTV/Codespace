dbf3f23e2bb5a768b0cf8792dd8c38c2a3ffc026
ArthurV0n <arthur.pedroso@sptech.school>
2024-03-15 12:44:43 -0300

    site/mainpage/navbar/menuHamburguer pronto

---
+++ site/public/js/MenuHamburguer.js
@@ -0,0 +1,8 @@
+function checar() {
+    if(check.checked==true){
+        menuItems.style.transform = "translate(300px,0)";
+    }else{
+        menuItems.style.transform = "translate(0px,0)";
+    }
+
+}
