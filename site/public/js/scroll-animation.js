c49a733ca2594cfb750d9f8864ac851ce97f2bd5
ArthurV0n <arthur.pedroso@sptech.school>
2024-03-15 14:20:26 -0300

    site/main/navegacao to login

--- site/public/js/scroll-animation.js
+++ site/public/js/scroll-animation.js
@@ -2,14 +2,6 @@ function irsegundaPagina() {
   window.scroll({top: window.innerHeight,behavior:'smooth'})
 }
 
-
-
-$('.navbar .container ul a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
-    var id = $(this).attr('href'),
-    targetOffset = $(id).offset().top;
-      
-    $('html, body').animate({ 
-      scrollTop: targetOffset - 100
-    }, 500);
-  });
+function irLogin(){
+    window.location.href = "login.html"
+}
