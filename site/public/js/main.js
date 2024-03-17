1b7361f8e9819ceeb00143e4db0e7578a8ff7b22
ArthurV0n <arthur.pedroso@sptech.school>
2024-03-17 17:17:46 -0300

    site/mainpage/planos terminado

--- site/public/js/main.js
+++ site/public/js/main.js
@@ -1,33 +1,37 @@
 function checar() {
-    if(check.checked==false){
-        menuItems.style.transform = "translate(0,-100vh)";
-      
-    }else{
-        menuItems.style.transform = "translate(0px,0px)";
-    }
+  if (check.checked == false) {
+    menuItems.style.transform = "translate(0,-100vh)";
+
+  } else {
+    menuItems.style.transform = "translate(0px,0px)";
+  }
 
 }
 
 function irsegundaPagina() {
-    window.scroll({top: window.innerHeight,behavior:'smooth'})
+  window.scroll({ top: window.innerHeight, behavior: 'smooth' })
+
+}
+function irquintaPagina() {
+  window.scroll({ top: window.innerHeight*4, behavior: 'smooth' })
+
+}
+
+function irLogin() {
+  window.location.href = "login.html"
+
+}
+
+celularInput.addEventListener('keypress', () => {
+
+  if (celularInput.value.length < 1) {
+    celularInput.value += '('
   }
-  
-  function irLogin(){
-      window.location.href = "login.html"
+  if (celularInput.value.length === 3) {
+    celularInput.value += ')'
   }
-  
-  celularInput.addEventListener('keypress', ()=>{
-  
-    if(celularInput.value.length<1){
-      celularInput.value += '('
-    }
-    if(celularInput.value.length===3){
-      celularInput.value += ')'
-    }
-  
-    if(celularInput.value.length=== 9){
-      celularInput.value += '.'
-    }
-  
-   
-  })
+  if (celularInput.value.length === 9) {
+    celularInput.value += '.'
+  }
+
+})
