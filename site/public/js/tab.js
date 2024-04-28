215896a6a171b44cc2d21048535eea5264de9348
MatheusKyoshiShoji <matheus.shoji@sptech.school>
2024-04-28 13:51:27 -0300

    site/configuracao-page v1

---
+++ site/public/js/tab.js
@@ -0,0 +1,23 @@
+// Tab para página configurações 
+
+function tabConfiguracao(evt, optionValue) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab-links");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(optionValue).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
+
+
+function editInput(inputId) {
+  var inputElement = document.getElementById(inputId);
+  inputElement.readOnly = !inputElement.readOnly;
+}
